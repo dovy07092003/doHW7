b5d599a915c6faf65853746b5a26a92f4df92f26
Demo User <debian@beaglebone.localdomain>
2023-11-06 01:04:27 +0000

    Temporary files for hw7

---
+++ build_myApp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Building for myApp.cpp
+g++ myApp.cpp -o myApp
+echo Done
+
+
---
+++ derek_LED.h
@@ -0,0 +1,24 @@
+// Name: derek_LED.h
+//This is the header file form the makeLESs.cpp
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class LED
+{
+      private:
+	   string path;
+	   int number;
+	   virtual void writeLED(string filename, string value);
+	   virtual void removeTrigger();
+					     
+      public:
+	   LED(int number);// Constructor (same name with the class)	
+	   virtual void turnOn();
+           virtual void turnOff();
+	   virtual void flash(string delayms);
+	   virtual void outputState();
+	   virtual ~LED();
+};
